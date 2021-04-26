2afa306941e7139e5a1671cb3c8a7a4644634f2f
Alisson Osvaldo <68975519+alisson-osvaldo@users.noreply.github.com>
2021-04-26 18:30:37 -0300

    Delete src/models directory

--- src/models/estoqueShema.ts
+++
@@ -1,8 +0,0 @@
-import { model, Schema } from"mongoose";
-import { entradaShema } from "./EntradaShema";
-
-const estoqueShema = new Schema ({
-
-});
-
-export default model("estoque", estoqueShema);
